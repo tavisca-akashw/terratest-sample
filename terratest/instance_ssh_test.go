280577834f0bd2247124dd1f00ac8072d6828e3d
Akash Warkhade <a.warkhade98@gmail.com>
2022-01-27 20:21:37 +0530

    updated test file

--- terratest/instance_ssh_test.go
+++ terratest/instance_ssh_test.go
@@ -1,19 +1,16 @@
-package test
+package terratest
 import (
    "testing"
-   "fmt"
-   "time"
    "github.com/stretchr/testify/assert"
    "github.com/gruntwork-io/terratest/modules/terraform"
-   "github.com/gruntwork-io/terratest/modules/aws"
-   "github.com/gruntwork-io/terratest/modules/ssh"
-   "github.com/gruntwork-io/terratest/modules/retry"
 )
 
 func TestInstanceSshKey(t *testing.T) {
-    terraformOptions := configureTerraformOptions(t)
+    terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+                TerraformDir: "../terraform",
+        })
     defer terraform.Destroy(t, terraformOptions)
     terraform.InitAndApply(t, terraformOptions)
     instanceSshKey := terraform.Output(t, terraformOptions, "instance_key")
-    assert.Equal(t, "terra_test_key", instanceSshKey)
+    assert.Equal(t, "terratest", instanceSshKey)
 }
