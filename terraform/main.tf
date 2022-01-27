13c01d8f40d4ae323f1e321579e1f14cb88560ac
Akash Warkhade <a.warkhade98@gmail.com>
2022-01-27 20:18:20 +0530

    updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
 resource "aws_security_group" "allow_ssh" {
+    name = "test-sg"
     ingress {
         from_port = "22"
         to_port   = "22"
@@ -9,14 +15,12 @@ resource "aws_security_group" "allow_ssh" {
 
 
 resource "aws_instance" "terratest_ec2" {
-    ami           = "ami-5026902d"
+    ami           = "ami-08e4e35cccc6189f4"
     instance_type = "t2.micro"
-    key_name = "terra_test_key"
-    vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}]"
+    key_name = "terratest"
+    security_groups = ["${aws_security_group.allow_ssh.name}"]
     user_data = <<-EOF
     #!/bin/bash
     echo 'Hello, World!' > /tmp/salut
     EOF
 }
-
-
