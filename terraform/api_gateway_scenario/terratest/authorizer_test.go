2b8fcaa724fe114920be5d7adbb682b87f53f389
Akash Warkhade <a.warkhade98@gmail.com>
2022-02-27 20:00:27 +0530

    updated

--- terraform/api_gateway_scenario/terratest/authorizer_test.go
+++ terraform/api_gateway_scenario/terratest/authorizer_test.go
@@ -1,11 +1,11 @@
 package terratest
 import (
-   "log"
-   "fmt"
    //"bytes"
+   "fmt"
+   "github.com/stretchr/testify/assert"
    terra_test "github.com/gruntwork-io/terratest/modules/testing"
    testing "testing"
-   "github.com/aws/aws-sdk-go/aws"
+   //"github.com/aws/aws-sdk-go/aws"
    terra_aws "github.com/gruntwork-io/terratest/modules/aws"
    "github.com/gruntwork-io/terratest/modules/terraform"
    "github.com/aws/aws-sdk-go/service/apigateway"
@@ -19,17 +19,16 @@ func TestApiGateway(t *testing.T) {
         })
         //defer terraform.Destroy(t, terraformOptions)
     terraform.InitAndApply(t, terraformOptions)
-    //stageName := terraform.Output(t, terraformOptions, "stage_name")
     apiId :=  terraform.Output(t, terraformOptions, "api_id")
     apiAuthorizer := GetAPIGwAuthorizers(t, awsRegion, apiId)
-    log.Println("Map: " + fmt.Sprint(apiAuthorizer))
-    //assert.Equal(t, stageName , actualStage)
+    apiAuthorizert := fmt.Sprint(apiAuthorizer)
+    assert.Equal(t, "TestAuthorizer" , apiAuthorizert)
 }
 
 
 
 
-func GetAPIGwAuthorizers(t terra_test.TestingT, awsRegion string, apiId string) map[string]string {
+func GetAPIGwAuthorizers(t terra_test.TestingT, awsRegion string, apiId string) []string {
         ApiAuthorizers, err := GetAPIGwAuthorizersE(t, awsRegion, apiId)
         require.NoError(t, err)
 
@@ -38,7 +37,7 @@ func GetAPIGwAuthorizers(t terra_test.TestingT, awsRegion string, apiId string) 
 
 
 
-func GetAPIGwAuthorizersE(t terra_test.TestingT, awsRegion string, apiId string) (map[string]string, error) {
+func GetAPIGwAuthorizersE(t terra_test.TestingT, awsRegion string, apiId string) ([]string, error) {
         apiGWClient, err := NewAPIgwClientE(t, awsRegion)
         if err != nil {
                 return nil, err
@@ -51,13 +50,12 @@ func GetAPIGwAuthorizersE(t terra_test.TestingT, awsRegion string, apiId string)
                 return nil, err
         }
 
-        authorizers := map[string]string{}
+        authorizers_list := []string{}
         for _, authorizer := range out.Items {
 
-                authorizers[aws.StringValue(authorizer.Name)] = aws.StringValue(authorizer.Type)
-                //authorizers[aws.StringValue(authorizer.Name)] = aws.StringValue(authorizer.Name)
+              authorizers_list = append(authorizers_list, *authorizer.Name)
         }
-        return authorizers, nil
+        return authorizers_list, err
 }
 
 
