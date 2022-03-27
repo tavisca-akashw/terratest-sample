77e8a81f330d3e6bbe028c857116d4772907d779
akash123-eng <a.warkhade98@gmail.com>
2022-03-27 15:39:22 +0000

    updated go files with go fmt

--- api_gateway/terratest/api_gateway_test.go
+++ api_gateway/terratest/api_gateway_test.go
@@ -1,31 +1,31 @@
 package terratest
+
 import (
-   "log"
-   "net/http"
-   "time"
-   "testing"
-   "github.com/stretchr/testify/assert"
-   "github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"net/http"
+	"testing"
+	"time"
 )
 
 func TestApiGateway(t *testing.T) {
-    terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-                TerraformDir: "../",
-        })
-    defer terraform.Destroy(t, terraformOptions)
-    terraform.InitAndApply(t, terraformOptions)
-    stageUrl := terraform.Output(t, terraformOptions,"deployment_invoke_url")
-    time.Sleep(30 * time.Second)
-    statusCode := DoGetRequest(t, stageUrl)
-    assert.Equal(t, 200 , statusCode)
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../",
+	})
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+	stageUrl := terraform.Output(t, terraformOptions, "deployment_invoke_url")
+	time.Sleep(30 * time.Second)
+	statusCode := DoGetRequest(t, stageUrl)
+	assert.Equal(t, 200, statusCode)
 }
 
-
-func DoGetRequest(t *testing.T, api string) int{
-   resp, err := http.Get(api)
-   if err != nil {
-      log.Fatalln(err)
-   }
-//We Read the response status on the line below.
-   return resp.StatusCode
+func DoGetRequest(t *testing.T, api string) int {
+	resp, err := http.Get(api)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response status on the line below.
+	return resp.StatusCode
 }
--- ec2_instance/terratest/instance_ssh_test.go
+++ ec2_instance/terratest/instance_ssh_test.go
@@ -1,16 +1,17 @@
 package terratest
+
 import (
-   "testing"
-   "github.com/stretchr/testify/assert"
-   "github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestEc2SshKey(t *testing.T) {
-    terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-                TerraformDir: "../",
-        })
-    defer terraform.Destroy(t, terraformOptions)
-    terraform.InitAndApply(t, terraformOptions)
-    ec2SshKey  := terraform.Output(t, terraformOptions, "instance_ssh_key")
-    assert.Equal(t, "terratest", ec2SshKey)
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../",
+	})
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+	ec2SshKey := terraform.Output(t, terraformOptions, "instance_ssh_key")
+	assert.Equal(t, "terratest", ec2SshKey)
 }
